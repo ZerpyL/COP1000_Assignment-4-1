168dc2e9b9bb602cb152d7820907e54ee2bd5fe5
Colby <128097306+ZerpyL@users.noreply.github.com>
2025-02-23 23:10:26 +0000

    Added Variables and if statements to check for total charge.

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ This program calculates prices for custom house signs.
 """
 
 # Declare and initialize variables here.
-# Charge for this sign.
-# Number of characters.
-# Color of characters.
-# Type of wood.
+charge = 35.00        
+numChars = 20  
+color = "gold"       
+woodType = "pine"    
+
 
 # Write assignment and if statements here as appropriate.
+if numChars > 5:
+    charge += (numChars - 5) * 4 
+
+
+if woodType == "oak":
+    charge += 20.00  
+elif woodType == "pine":
+    pass  
+
+if color == "gold":
+    charge += 15.00
 
 # Output Charge for this sign.
 print("The charge for this sign is $" + str(charge))
